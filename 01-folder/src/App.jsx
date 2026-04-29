f5813d77398937b2b0d528e370f9f63077e61928
rahulsargar3069 <rahulsargar3069@gmail.com>
2026-04-29 20:28:14 +0530

    Initial commit with 01-folder project

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, architecto?</p>
+    </div>
+  )
+}
+
+export default App
